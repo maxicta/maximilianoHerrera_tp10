e13bb839c445019788cf06b59aca96573a159e18
maxicta <herreramaximiliano@hotmail.com>
2024-10-30 14:33:16 -0300

    operaciones.js
    se agrego file operaciones.js

---
+++ boletos.js
@@ -0,0 +1,18 @@
+let nombre = "Fulanito"
+let apellido = "Menganito"
+let edad = 25
+let numeroTelefono = 8008881600
+let socio = true
+let fechaPartido = "28 enero de 2025"
+let numeroSilla = "26b"
+let precioBoleto = 25.50
+
+console.log(nombre , "es un valor" , typeof nombre);
+console.log(apellido , "es un valor" , typeof apellido);
+console.log(edad , "es un valor" , typeof edad);
+console.log(numeroTelefono , "es un valor" , typeof numeroTelefono);
+console.log(socio , "es un valor" , typeof socio);
+console.log(fechaPartido , "es un valor" , typeof fechaPartido);
+console.log(numeroSilla , "es un valor" , typeof numeroSilla);
+console.log(precioBoleto , "es un valor" , typeof precioBoleto);
+
